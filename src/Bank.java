efdf1c1c54656a80469dd48c25cdfcbd5317fa23
manuraj23 <manuraj082004@gmail.com>
2026-08-05 10:13:15 +0530

    Basic Files added

---
+++ src/Bank.java
@@ -0,0 +1,36 @@
+package src;
+import java.util.*;
+public class Bank {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Welcome to ABC bank");
+        int balance = 0; 
+        int option=1;
+        while(option != 9){
+            System.out.println("\n--- MAIN MENU ---");
+            System.out.println("Enter your choice");
+            System.out.println("1. Check Balance");
+            System.out.println("2. Money Deposit");
+            System.out.println("3. Withdraw Money");
+            System.out.println("9. Exit");
+
+            System.out.print("Enter your choice: ");
+            option=sc.nextInt();
+            System.out.println();
+
+            switch(option){
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 9:
+                    System.out.println("Thank you for using ABC Bank!");
+                    break;
+                default:
+                    System.out.println("Invalid choice- Enter choice from main menu");
+            }
+        }
+    }
+}
